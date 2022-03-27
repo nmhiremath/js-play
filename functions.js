6fca501032aa7ef4c3b084b6b5aa16b040a1de7e
Nag Hiremath <nag_hiremath@epam.com>
2022-03-27 17:24:34 -0400

    Functions

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Performing a task
+function greet(fname, lname = ''){
+    console.log("Hello, " + fname + " " + lname);
+}
+
+// Calculates the square of input number
+function square(number){
+    return number ** 2;
+}
+
+console.log(square(2))
