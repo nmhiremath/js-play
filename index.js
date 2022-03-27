b39f571e157f074fd019a7cc788fefd2448359c9
Nag Hiremath <nag_hiremath@epam.com>
2022-03-27 16:10:31 -0400

    JS CC - Init

---
+++ index.js
@@ -0,0 +1,15 @@
+// Commie!
+
+console.log('JS On console. In it\'s own file!')
+console.log('Now running on node!!')
+
+let name = 'Bond, James';
+console.log(name);
+
+let firstName = "James";
+let lastName = "Bond";
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
+
